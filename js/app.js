084305eea6103abdc961203cefd2820fb6aaafdb
Joe Nelson <joeynelson13@gmail.com>
2022-09-18 21:14:41 -0500

    Update all files and folders to finish product
    added all functionality to clear grid, change pen color,
    change to rainbow pen color mode. Adjusted slider for better
    functionality and for text under slider to update as slider changes.
    Added page title with name of website, added favicon, added cursor
    change when in earser mode, added footer for copyright info.

--- js/app.js
+++ js/app.js
@@ -1,55 +1,51 @@
 const GRID_CONTAINER = document.querySelector('.grid-container');
-const SAVE_BTN = document.querySelector('.save-btn');
+let mouseDown = false;
+let colorMode = 'color';
 
-
-//settings window and grid square event listeners
-const SETTINGS_BTN = document.querySelector('.settings-btn');
-const SETTINGS_WINDOW = document.querySelector('.settings-popup-window');
-const SETTINGS_CLOSE_BTN = document.querySelector('.close-settings-btn');
-const OVERLAY = document.querySelector('.overlay');
-
-let COLUM_CONTAINER = '';
-let GRID_SQUARES = '';
-
-
+//creats grid on page load and gives it a default size of 16
 createGrid(16);
 
 function createGrid(size) {
-    for (let i = 0; i < size; i++)
+    GRID_CONTAINER.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    GRID_CONTAINER.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    for (let i = 0; i < size * size; i++)
     {
-        COLUM_CONTAINER = document.createElement('div');
-        COLUM_CONTAINER.classList.add('colum-container');
-        GRID_CONTAINER.appendChild(COLUM_CONTAINER);
-        for (let j = 0; j < size; j++)
-        {
-            const GRID_SECTION = document.createElement('div');
-            GRID_SECTION.classList.add('grid-section');
-            COLUM_CONTAINER.appendChild(GRID_SECTION);
-        }
+        const GRID_SQUARE = document.createElement('div');
+        GRID_CONTAINER.appendChild(GRID_SQUARE);
+        GRID_SQUARE.addEventListener('mouseup', () => {
+            mouseDown = false;
+        });
+        GRID_SQUARE.addEventListener('mousedown', () => {
+            mouseDown = true;
+        });
+        
+        GRID_SQUARE.addEventListener('mouseover', draw);
+    }
+}
+
+//chooses color to draw with based on button selection
+//returns right away if mouseDown is false allowing user not to draw
+function draw(e) {
+    if (mouseDown === false)
+    {
+        return;
     }
 
-    GRID_SQUARES = document.querySelectorAll('.grid-section');
-    hoverGrid();
+    if (colorMode === 'color')
+    {
+        e.target.style.backgroundColor = COLOR_PICKER.value;
+    }
+    else if (colorMode === 'rainbow')
+    {
+        e.target.style.backgroundColor = `rgb(${createRainbow()}, ${createRainbow()}, ${createRainbow()})`;
+    }
+    else //eraser
+    {
+        e.target.style.backgroundColor = '';
+    }
+}
     
-}
-
-
-
-function hoverGrid()
-{
-    GRID_SQUARES.forEach((square) => {
-        square.addEventListener('mouseover', () => {
-            //checks if settings window is open and stops evenlistener from working
-            if (SETTINGS_WINDOW.classList.contains('settings-popup-active'))
-            {
-                return;
-            }
-            square.classList.toggle('grid-square-hover');
-        });
-    });
-}
-
-function resetGrid()
+function removeOldGrid()
 {
     while (GRID_CONTAINER.hasChildNodes())
     {
@@ -57,22 +53,67 @@ function resetGrid()
     }
 }
 
+//generates number from 0-255 for r,g,and b values
+function createRainbow ()
+{
+    return Math.floor(Math.random() * 256);
+}
 
+const COLOR_PICKER = document.querySelector('.color-input');
 
-SETTINGS_BTN.addEventListener('click', () => {
-    SETTINGS_WINDOW.classList.add('settings-popup-active');
-    OVERLAY.classList.add('overlay-active');
+COLOR_PICKER.addEventListener('change', () => {
+    COLOR_PICKER.parentNode.style.backgroundColor = COLOR_PICKER.value;
 });
 
-SETTINGS_CLOSE_BTN.addEventListener('click', () => {
-    SETTINGS_WINDOW.classList.remove('settings-popup-active');
-    OVERLAY.classList.remove('overlay-active');
+//COLOR BUTTON
+const COLOR_BTN = document.querySelector('.mode-button.color');
+COLOR_BTN.style.backgroundColor = '#374785';
+
+COLOR_BTN.addEventListener('click', () => {
+    colorMode = 'color';
+    COLOR_BTN.style.backgroundColor = '#374785';
+    RAINBOW_BTN.style.backgroundColor = '';
+    ERASER_BTN.style.backgroundColor = '';
+    GRID_CONTAINER.style.cursor = 'default';
 });
 
-SAVE_BTN.addEventListener('click', () => {
-    let gridSize = document.querySelector('.input-size').value;
-    resetGrid();
-    createGrid(gridSize);
-    SETTINGS_WINDOW.classList.remove('settings-popup-active');
-    OVERLAY.classList.remove('overlay-active');
+//RAINBOW BUTTON
+const RAINBOW_BTN = document.querySelector('.mode-button.rainbow');
+
+RAINBOW_BTN.addEventListener('click', () => {
+    colorMode = 'rainbow';
+    RAINBOW_BTN.style.backgroundColor = '#374785';
+    COLOR_BTN.style.backgroundColor = '';
+    ERASER_BTN.style.backgroundColor = '';
+    GRID_CONTAINER.style.cursor = 'default';
+});
+
+//ERASER BUTTON
+const ERASER_BTN = document.querySelector('.mode-button.eraser');
+
+ERASER_BTN.addEventListener('click', () => {
+    colorMode = 'eraser';
+    ERASER_BTN.style.backgroundColor = '#374785';
+    COLOR_BTN.style.backgroundColor = '';
+    RAINBOW_BTN.style.backgroundColor = '';
+    GRID_CONTAINER.style.cursor = 'url("images/eraser_cursor.svg"), default';
+});
+
+//CLEAR BUTTON
+const CLEAR_BTN = document.querySelector('.mode-button.clear');
+
+CLEAR_BTN.addEventListener('click', () => {
+    removeOldGrid();
+    createGrid(SLIDER_SIZE.value);
+    GRID_CONTAINER.style.cursor = 'default';
+});
+
+// SLIDER
+const SLIDER_SIZE = document.querySelector('.grid-size-slider');
+const GRID_SIZE_TEXT = document.querySelector('.grid-size-text');
+
+SLIDER_SIZE.addEventListener('input', () => {
+    GRID_SIZE_TEXT.textContent = `${SLIDER_SIZE.value} x ${SLIDER_SIZE.value}`;
+    removeOldGrid();
+    createGrid(SLIDER_SIZE.value);
 });
